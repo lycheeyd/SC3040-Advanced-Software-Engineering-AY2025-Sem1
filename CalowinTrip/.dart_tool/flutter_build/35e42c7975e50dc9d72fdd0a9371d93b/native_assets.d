0c3912afe4a57f2e2a3a54267215c9287e02ef6f
unknown <165633195+lycheeyd@users.noreply.github.com>
2025-09-25 17:43:13 +0800

    Removed unnecessary files in CalowinTrip

--- CalowinTrip/.dart_tool/flutter_build/35e42c7975e50dc9d72fdd0a9371d93b/native_assets.d
+++
@@ -1,1 +0,0 @@
- C:\\Users\\junji\\Downloads\\Calowin\\Calowin\\.dart_tool\\flutter_build\\35e42c7975e50dc9d72fdd0a9371d93b\\native_assets.yaml: 
--- CalowinTrip/lib/models/location.dart
+++
@@ -1,9 +0,0 @@
-// lib/models/location.dart
-
-class Location {
-  final String name;
-  final double latitude;
-  final double longitude;
-
-  Location(this.name, this.latitude, this.longitude);
-}
