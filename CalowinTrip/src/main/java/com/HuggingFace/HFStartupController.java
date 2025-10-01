7503e5c3ed1a8816c55a2bee5c4b0bbdc2e30056
lycheeyd <165633195+lycheeyd@users.noreply.github.com>
2025-10-01 23:40:35 +0800

    Update HFStartupController.java
    Removed unused import

--- CalowinTrip/src/main/java/com/HuggingFace/HFStartupController.java
+++ CalowinTrip/src/main/java/com/HuggingFace/HFStartupController.java
@@ -1,6 +1,5 @@
 package com.HuggingFace;
 
-import com.controller.HttpReqController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
