8624969e1bbc8e06d0cb3c8da5b462336c377a15
liuconggg <128717856+liuconggg@users.noreply.github.com>
2025-10-13 22:53:18 +0800

    added missing package to CalowinNotification main file

--- CalowinNotification/src/main/java/com/CalowinNotification.java
+++ CalowinNotification/src/main/java/com/CalowinNotification.java
@@ -6,7 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EntityScan(basePackages = "com.RelationshipNotification.Entity")
 
-@SpringBootApplication(scanBasePackages = { "com.Relationship", "com.Database", "com.RestController" })
+@SpringBootApplication(scanBasePackages = {
+        "com.Relationship",
+        "com.Database",
+        "com.RestController",
+        "com.RelationshipNotification"
+
+})
 public class CalowinNotification {
     public static void main(String[] args) {
         SpringApplication.run(CalowinNotification.class, args);
