ad4066c08f6d804cd906d01282da5ceb5c776538
liuconggg <128717856+liuconggg@users.noreply.github.com>
2025-10-21 14:39:49 +0800

    update deployment workflow

--- CalowinNotification/src/main/java/com/CalowinNotification.java
+++ CalowinNotification/src/main/java/com/CalowinNotification.java
@@ -3,7 +3,7 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
+//
 @EntityScan(basePackages = "com.RelationshipNotification.Entity")
 
 @SpringBootApplication(scanBasePackages = {
